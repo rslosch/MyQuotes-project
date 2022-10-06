76e3781683c2ba04b060a3fb9a9f4780c49a455f
Steven <slosch99@gmail.com>
2022-10-06 17:35:17 -0500

    "Add routes in App for Signup and Login and create rafce in components. Create login, logout, signup functions in context"

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css';
 import Navbar from './Navbar'
 import Home from './Home'
+import Signup from './Signup'
+import Login from './Login'
 import { UserProvider } from './context/user'
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/signup" element={<Signup />} />
+          <Route exact path="/login" element={<Login />} />
         </Routes>
       </UserProvider>
     </div>
