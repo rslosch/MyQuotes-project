2c92e28ccb2d5f8326de9d3736a66bf83dbc8c14
Steven <slosch99@gmail.com>
2022-10-06 17:16:52 -0500

    Add show and create action logic handling for signup feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,26 @@ class UsersController < ApplicationController
 
     #signup
     def create
-
+        user = User.create(user_params)
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
+        end
     end
 
     #get which user is logged in
     def show
-        byebug
         #Get current user
-
+        user = User.find_by(id: session[:user_id])
+        render json: user, status: :created
     end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
+    end
+
 end
