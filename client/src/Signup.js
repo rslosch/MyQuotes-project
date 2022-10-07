0f5d54509286489c6bab6b4c417c53d5cdaffde3
Steven <slosch99@gmail.com>
2022-10-07 12:54:18 -0500

    "Build controlled signup form and hooked signup function from UserContext"

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import { UserContext } from './context/user'
 
 const Signup = () => {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [passwordConfirmation, setPasswordConfirmation] = useState("")
+    const [errorsList, setErrorsList] = useState([])
+    const { signup } = useContext(UserContext)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
   return (
-    <div>Signup</div>
+    <div>
+        <form onSubmit={handleSubmit}>
+            <label> Username: </label>
+            <input 
+                type="text"
+                id="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            /> <br />
+            <label> Password: </label>
+            <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            /> <br />
+            <label> Confirm Password: </label>
+            <input
+                type="password"
+                id="password_confirmation"
+                value={passwordConfirmation}
+                onChange={(e) => setPasswordConfirmation(e.target.value)}
+            /> <br />
+            <input type="submit" />
+        </form>
+        <ul>
+            {errorsList}
+        </ul>
+    </div>
   )
 }
 
