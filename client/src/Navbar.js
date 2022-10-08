2bd95fb311249ce36b06d03fa9c521e098bc8fe2
Steven <slosch99@gmail.com>
2022-10-08 15:44:06 -0500

    Add NavLink buttons for Login and Signup

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,10 +1,41 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { NavLink, useNavigate } from 'react-router-dom'
+import { UserContext } from './context/user'
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+  const {user, logout} = useContext(UserContext)
+
+  // const navigate = useNavigate()
+
+  const logoutUser = () => {
+    fetch('/logout', {
+      method: 'DELETE',
+    })
+    .then(() => {
+      logout()
+      // navigate('/login')
+    })
+  }
+
+  if(user){
+    return (
+      <div>
+        <h1> Hello {user.username} </h1>
+        <button onClick={logoutUser}>Logout</button>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <NavLink to='/login'>
+          <button>Login</button>  
+        </NavLink>
+        <NavLink to='/signup'>
+          <button>Signup</button>  
+        </NavLink>
+      </div>
+    )
+  }
 }
 
 export default Navbar
