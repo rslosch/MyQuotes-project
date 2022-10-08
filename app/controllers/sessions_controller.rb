9525e102ec307076f4113b5c585232e08a400747
Steven <slosch99@gmail.com>
2022-10-08 15:46:52 -0500

    Add SessionsController logic for login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,21 @@
 class SessionsController < ApplicationController
-    
+
+    skip_before_action :authorize
     #login
     def create
-
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: { error: "Invalid username or password"}, status: :unauthorized
+        end
     end
 
     #logout
     def destroy
-
+        # byebug
+        session.clear
+        head :no_content
     end
 end
