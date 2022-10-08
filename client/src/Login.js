ad8548a5e63295965112219141c352dd250d5226
Steven <slosch99@gmail.com>
2022-10-08 13:57:39 -0500

    Add controlled Login form

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import { UserContext } from './context/user'
 
 const Login = () => {
+
+  const [username, setUsername] = useState("")
+  const [password, setPassword]= useState("")
+  const [error, setError] = useState("")
+
+  const {login} = useContext(UserContext)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>Login</div>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Username: </label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+          /> <br/>
+        <label>Password: </label>
+        <input 
+          type="password"
+          id="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        /> <br/>
+        <input type="submit"/>
+      </form>
+      <ul>
+        {error}
+      </ul>
+    </div>
   )
 }
 
