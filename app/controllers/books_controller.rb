3cf2d0500511e34bb8999a3a35fa6ef4d1b5a63d
Steven <slosch99@gmail.com>
2022-10-12 18:09:04 -0400

    Full crud actions for BooksController and validation to model

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,51 @@
 class BooksController < ApplicationController
 
     def create
-
+        book = current_user.books.create(book_params)
+        if books.valid?
+            render json: book
+        else
+            render json: { errors: book.errors.full_messages }, status: :unprocessable_entity
+        end
     end
 
     def index
-
+        books = current_user.books
+        render json: books
     end
 
     def show
+        book = current_user.books.find_by(id: params[:id])
+        if book
+            render json: book
+        else
+            render json: { error: "Not Found" }, status: :unauthorized
+        end
+    end
 
+    def update
+        book = current_user.books.find_by(id: params[:id])
+        if book
+            book.update(book_params)
+            render json: book
+        else
+            render json: { error: "Not Found" }, status: :unauthorized
+        end
     end
 
     def destroy
-        
+        book = current_user.books.find_by(id: params[:id])
+        book.destroy
+    end
+
+    private
+
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+
+
+    def book_params
+        params.permit(:title, :author)
     end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,7 @@
 class Book < ApplicationRecord
     has_many :excerpts
     has_many :users, through: :excerpts
+
+    validates :author, :title, presence: true
+
 end
