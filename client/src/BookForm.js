70f9214cc91f3dc9c32df87e9b96b3d56d7bf2f8
Steven <slosch99@gmail.com>
2022-10-19 11:59:47 -0400

    Controlled form for Books and rendering of BookForm within Books component

---
+++ client/src/BookForm.js
@@ -0,0 +1,40 @@
+import React, { useState, useContext } from 'react'
+import { UserContext } from './context/user'
+
+const BookForm = () => {
+    const [author, setAuthor] = useState("")
+    const [title, setTitle] = useState("")
+    const { addBook } = useContext(UserContext)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        addBook({
+            author: author,
+            title: title
+        })
+        setAuthor("")
+        setTitle("")
+    }
+ 
+  return (
+    <form onSubmit={handleSubmit}>
+        <label>Title: </label>
+        <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+        /> <br/>
+        <label>Author: </label>
+        <input 
+            type="text"
+            id='author'
+            value={author}
+            onChange={e => setAuthor(e.target.value)}
+        /> <br/>
+        <input type="submit"/>
+    </form>
+  )
+}
+
+export default BookForm
--- client/src/Books.js
+++ client/src/Books.js
@@ -6,6 +6,7 @@ import { UserContext } from './context/user'
 const Books = () => {
 
     const { loggedIn, books } = useContext(UserContext)
+    const [formFlag, setFormFlag] = useState(false)
     const params = useParams()
     
     if (loggedIn) {
@@ -15,6 +16,12 @@ const Books = () => {
                 <h1>Books </h1>
                 <br/>
                 {booksList}
+                <br/>
+                {formFlag ?
+                    <BookForm />
+                    :
+                    <button onClick={() => setFormFlag(true)}>Add Book</button>
+                }
             </div>
 
             
