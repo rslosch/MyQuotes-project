6cb1e79d59c1d6d15d0a878ac961cca01d091537
Steven <slosch99@gmail.com>
2022-11-02 15:18:06 -0400

    Fix fixed props sent to ExcerptOnlyForm for posting

--- client/src/Book.js
+++ client/src/Book.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState, useContext } from 'react'
 import { useParams } from 'react-router-dom'
 import { UserContext } from './context/user'
+import ExcerptList from './ExcerptList'
+import ExcerptOnlyForm from './ExcerptOnlyForm'
 
 const Book = () => {
 
+    const [formFlag, setFormFlag] = useState(false)
     const params = useParams()
-    const { showBook, currentBook } = useContext(UserContext)
+    const { showBook, currentBook, fetchExcerpts, excerpts } = useContext(UserContext)
 
     useEffect(() => {
         showBook(params.id)
@@ -15,6 +18,14 @@ const Book = () => {
     <div>
         <h2>{currentBook.title}</h2>
         <h3>By: {currentBook.author}</h3>
+        <br />
+        {/* {excerptsList} */}
+        {formFlag ?
+          <ExcerptOnlyForm paramBookId={params.id} />
+          :
+          <button onClick={() => setFormFlag(true)}>Add Excerpt</button>
+        }
+        
     </div>
   )
 }
