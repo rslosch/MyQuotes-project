9c2e009f3f9453e419b6ec09bfe9d90c532f7bec
Steven <slosch99@gmail.com>
2022-11-08 10:55:42 -0500

    Move map into else logic

--- client/src/Home.js
+++ client/src/Home.js
@@ -7,17 +7,16 @@ const Home = () => {
     const  { user, loggedIn } = useContext(UserContext)
     // const navigate = useNavigate()
 
-    const myBooksList = user.unique_books.map(b => {
-        return (
-        <li key={b.id}> {b.title}</li>
-        )
-    })
-
     if(!loggedIn){
         return (
             <h1> Please Login or Signup </h1>
         )
     } else {
+        const myBooksList = user.unique_books.map(b => {
+            return (
+            <li key={b.id}> {b.title}</li>
+            )
+        })
         return (
             <div>
                 <h1>{user.username}'s Homepage</h1>
