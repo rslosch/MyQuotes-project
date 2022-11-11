705665b905f730871e810762523a74c29eaa93d5
Steven <slosch99@gmail.com>
2022-11-11 16:01:39 -0500

    MUI list style for Books component

--- client/src/Books.js
+++ client/src/Books.js
@@ -4,6 +4,14 @@ import Book from './Book'
 import EditBookForm from './EditBookForm'
 import { Link } from 'react-router-dom'
 import { UserContext } from './context/user'
+import Typography from '@material-ui/core/Typography'
+import Box from '@material-ui/core/Box'
+import Grid from '@material-ui/core/Grid'
+import LocalLibraryIcon from '@material-ui/icons/LocalLibrary';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 
 const Books = () => {
 
@@ -12,16 +20,26 @@ const Books = () => {
     if (loggedIn) {
         const booksList = books.map(b => {
             return (
-                <li key={b.id}>{b.title}: {b.author}</li>
+                <ListItem>
+                    <ListItemIcon>
+                        <LocalLibraryIcon />
+                    </ListItemIcon>
+                    <ListItemText primary={b.title} secondary={b.author}/>
+                </ListItem>
+                // <li key={b.id}>{b.title}: {b.author}</li>
             )
         })   
         return (
-            <div>
-                <h1>Library of all Books </h1>
+            <Box m={4}>
+                <Typography variant="h3">Library of all Books </Typography>
                 <br/>
-                {booksList}
+                <Grid container justifyContent="center">
+                    <List>
+                        {booksList}
+                    </List>
+                </Grid>
                 <br/>
-            </div>
+            </Box>
         )
     } else {
         return (
