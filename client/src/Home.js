2646212e5759d19ca65963e03fd7e04343681870
Steven <slosch99@gmail.com>
2022-11-11 16:09:52 -0500

    Add MUI style to Home component

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,6 +1,13 @@
 import React, { useContext } from 'react'
 import { UserContext } from './context/user'
 import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box'
+import Grid from '@material-ui/core/Grid'
+import LocalLibraryIcon from '@material-ui/icons/LocalLibrary';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 
 const Home = () => {
     const  { user, loggedIn } = useContext(UserContext)
@@ -16,17 +23,33 @@ const Home = () => {
     } else {
         const myBooksList = user.unique_books.map(b => {
             return (
-            <li key={b.id}> {b.title}</li>
+                <ListItem>
+                    <ListItemIcon>
+                        <LocalLibraryIcon />
+                    </ListItemIcon>
+                    <ListItemText primary={b.title} secondary={b.author}/>
+                </ListItem>
+            // <li key={b.id}> {b.title}</li>
             )
         })
         return (
-            <div>
-                <h1>{user.username}'s Homepage</h1>
-                <h2> Welcome to your MyQuotes Library App! </h2>
-                <h4> Easily store and record memorable quotes from your recently read books.</h4>
-                <h4> View your books below and Visit the My Quotes tab to track!</h4>
-                {myBooksList}
-            </div>
+            <Box m={4}>
+                <Grid container justifyContent='center'>
+                    <Typography variant="h2">{user.username}'s Homepage</Typography>
+                </Grid>
+                <Box m={4}>
+                    <Grid container justifyContent='center'>
+                        <Typography variant="h3"> Welcome to your MyQuotes Library App! </Typography>
+                        <Typography variant="h5"> Easily store and record memorable quotes from your recently read books.</Typography>
+                        <Typography variant="h5"> View your books below and visit the MyQuotes tab to track!</Typography>
+                    </Grid>
+                </Box>
+                <Grid container justifyContent="center">
+                    <List>
+                        {myBooksList}
+                    </List >
+                </Grid>
+            </Box>
         )
      }
 }
