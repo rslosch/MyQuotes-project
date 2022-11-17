ede595bf9fde06f1e802c2bfe8c6b5b97aba0737
Steven <slosch99@gmail.com>
2022-11-17 15:29:19 -0500

    Validate and dependent destroy method add

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,10 +1,10 @@
 class Book < ApplicationRecord
-    has_many :excerpts
+    has_many :excerpts, dependent: :destroy
     has_many :users, through: :excerpts
 
     accepts_nested_attributes_for :excerpts
 
     validates :author, :title, presence: true
-    # validates :title, uniqueness: true
+    validates :title, uniqueness: true
 
 end
